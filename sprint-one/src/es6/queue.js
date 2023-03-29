361cb50d3bebeeb492dd513a1b043c5b4630493a
XintongTheCoder <xintongmi@gmail.com>
2023-03-29 15:32:29 -0700

    complete both queue.js and stack.js with ES6 instantiation pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {
+      head: 0,
+      tail: 0,
+    };
   }
 
+  enqueue(value) {
+    var storage = this.storage;
+    storage[storage.tail] = value;
+    storage.tail++;
+  }
+
+  dequeue() {
+    var storage = this.storage;
+    if (storage.head === storage.tail) {
+      return;
+    }
+    var res = storage[storage.head];
+    delete storage[storage.head];
+    storage.head++;
+    return res;
+  }
+
+  size() {
+    return this.storage.tail - this.storage.head;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {
+      size: 0,
+    };
   }
 
+  push(value) {
+    var storage = this.storage;
+    storage[storage.size] = value;
+    storage.size++;
+  }
+
+  pop() {
+    var storage = this.storage;
+    if (storage.size === 0) {
+      return;
+    }
+    var res = storage[storage.size - 1];
+    delete storage[storage.size - 1];
+    storage.size--;
+
+    return res;
+  }
+
+  size() {
+    return this.storage.size;
+  }
 }
