d6de060c8f536919a0e7d225fe462da17af04d42
XintongTheCoder <xintongmi@gmail.com>
2023-03-29 12:18:08 -0700

    complete queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,4 +1,4 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,13 +6,32 @@ var Queue = function() {
 
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    if (!('head' in storage)) {
+      storage.head = 0;
+      storage.tail = 0;
+    }
+    storage[storage.tail] = value;
+    storage.tail++;
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+    // If Queue is empty, do nothing
+    if (!('head' in storage) || storage.head === storage.tail) {
+      return;
+    }
+
+    var res = storage[storage.head];
+    delete storage[storage.head];
+    storage.head++;
+    return res;
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    if (!('head' in storage)) {
+      return 0;
+    }
+    return storage.tail - storage.head;
   };
 
   return someInstance;
