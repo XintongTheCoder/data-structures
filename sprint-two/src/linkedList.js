22c2bffc771ecf7cd1c966df135f5ff42cee29d3
XintongTheCoder <xintongmi@gmail.com>
2023-03-29 21:37:28 -0700

    complete linkedList.js with functional pattern

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,46 @@
-var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
+var LinkedList = function () {
+  var list = {
+    head: null,
+    tail: null,
+  };
+  // list.head = null;
+  // list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    if (list.head === null) {
+      return;
+    }
+    var temp = list.head;
+    list.head = list.head.next;
+    temp.next = null;
+    return temp.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
+    var pointer = list.head;
+    while (pointer !== null) {
+      if (pointer.value === target) {
+        return true;
+      }
+      pointer = pointer.next;
+    }
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
